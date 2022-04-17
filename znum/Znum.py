d8b3d42d9005c8211e7e1235af8e671539758047
MahammadNuriyev62 <maganuriyev@gmail.com>
2022-04-17 13:36:59 +0400

    Sort logic added

--- znum/Znum.py
+++ znum/Znum.py
@@ -2,6 +2,7 @@ from scipy import optimize
 from numpy import linalg, array
 # from Replacer import replacer_reverse, round_sig
 from znum.Math import Math
+from znum.Sort import Sort
 
 
 class Znum:
@@ -13,6 +14,7 @@ class Znum:
         self.A_int = None  # self.get_intermediate(A)
         self.B_int = None  # self.get_intermediate(B)
         self.math = Math(self)
+        self.sort = Sort(self)
 
     def __str__(self):
         return f"Znum(A={self.A}, B={self.B})"
@@ -34,3 +36,33 @@ class Znum:
 
     def __pow__(self, power, modulo=None):
         return Znum(A=[a**power for a in self.A], B=self.B.copy())
+
+    def __gt__(self, o):
+        o: Znum
+        d, do = self.sort.main_solver(self, o)
+        _d, _do = self.sort.main_solver(o, self)
+        return do > _do
+
+    def __lt__(self, o):
+        o: Znum
+        d, do = self.sort.main_solver(self, o)
+        _d, _do = self.sort.main_solver(o, self)
+        return do < _do
+
+    def __eq__(self, o):
+        o: Znum
+        d, do = self.sort.main_solver(self, o)
+        _d, _do = self.sort.main_solver(o, self)
+        return do == 1 and _do == 1
+
+    def __ge__(self, o):
+        o: Znum
+        d, do = self.sort.main_solver(self, o)
+        _d, _do = self.sort.main_solver(o, self)
+        return do >= _do
+
+    def __le__(self, o):
+        o: Znum
+        d, do = self.sort.main_solver(self, o)
+        _d, _do = self.sort.main_solver(o, self)
+        return do <= _do
